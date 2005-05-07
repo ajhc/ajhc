2cde31e44794e1d64dc310ce9437dae0042fe905
Einar Karttunen <ekarttun@cs.helsinki.fi>
2005-05-07 22:43:24 +0900

    Haddock documentation for UniqueMonad.hs
    darcs-hash:20050507134324-e6152-900c4291b9b5aec3ba1c8f3d2d714da9fca203c3.gz

--- UniqueMonad.hs
+++ UniqueMonad.hs
@@ -1,7 +1,7 @@
 module UniqueMonad(UniqT,Uniq, runUniq, runUniqT, execUniq1, execUniq, execUniqT) where
 
 
-import GenUtil 
+import GenUtil
 import Data.Unique
 import Control.Monad.State
 import Control.Monad.Reader
@@ -19,28 +19,36 @@ instance Monad m =>  UniqueProducer (UniqT m) where
         modify (+1)
         get
 
+-- | Run the transformer version of the unique int generator.
 runUniqT :: Monad m =>  UniqT m a -> Int -> m (a,Int)
 runUniqT (UniqT sm) s  = runStateT sm s
 
+-- | Run the bare version of the unique int generator.
 runUniq :: Int -> Uniq a -> (a,Int)
 runUniq x y = runIdentity $ runUniqT y x
 
+-- | Execute the bare unique int generator starting with 1.
+execUniq1 :: Uniq a -> a
 execUniq1 x = fst $ runUniq 1 x  
+
+-- | Execute the bare unique int generator starting with the suplied number.
+execUniq :: Int -> Uniq a -> a
 execUniq st x = fst $ runUniq st x  
 
+-- | Execute the transformer version of the unique int generator starting with the suplied number.
 execUniqT :: Monad m =>  Int -> UniqT m a -> m a
 execUniqT s (UniqT sm)  = liftM fst $ runStateT sm s
 
 instance (Monad m, Monad (t m), MonadTrans t, UniqueProducer m) => UniqueProducer (t m) where
     newUniq = lift newUniq
 
+-- | Unique integer generator monad transformer.
 newtype UniqT m a = UniqT (StateT Int m a)
     deriving(Monad,  MonadTrans, Functor, MonadFix, MonadPlus)
 
 instance MonadReader s m => MonadReader s (UniqT m) where
     ask = UniqT $  ask
     local f (UniqT x) = UniqT $ local f x
-    
+
+-- | Unique integer generator monad.
 type Uniq a = UniqT Identity a
---newtype Uniq a = Uniq (UniqT Identity)
---    deriving(Monad,UniqueProducer)
