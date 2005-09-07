879dee701b1d19e13fefbf1c6c174bbbbbe79256
John Meacham <john@repetae.net>
2005-09-07 09:56:37 +0900

    add better HasSize instances for list
    darcs-hash:20050907005637-1a7c6-7669acfaed77e58b4fdeed7e0120fd7c34625579.gz

--- HasSize.hs
+++ HasSize.hs
@@ -1,10 +1,17 @@
-
 module HasSize where
+
+-- this point of this module is not only to share the 'size' syntax, but to
+-- provide optimally lazy versions of size comparasin functions when dealing
+-- with lazy structures. This is especially useful when having to compare the
+-- size of possibly long lists.
+
+-- it is up to each instance to decide what 'size' means
 
 import qualified Data.Map(Map,size)
 import qualified Data.Set(Set,size)
 import qualified Data.IntMap(IntMap,size)
 import qualified Data.IntSet(IntSet,size)
+
 
 class HasSize a where
     size :: a -> Int
@@ -24,10 +31,22 @@ genSize = fromIntegral . HasSize.size
 
 instance HasSize [x] where
     size = length
-    sizeEQ 0 [] = True
-    sizeEQ _ [] = False
-    sizeEQ 0 _ = False
-    sizeEQ n (_:xs) = sizeEQ (n - 1) xs
+    sizeEQ n _ | n < 0 = False
+    sizeEQ n xs = f n xs where
+        f 0 [] = True
+        f _ [] = False
+        f 0 _ = False
+        f n (_:xs) = sizeEQ (n - 1) xs
+    sizeGT n _ | n < 0 = True
+    sizeGT n xs = f n xs where
+        f 0 (_:_) = True
+        f n [] = False
+        f n (_:xs) = f (n - 1) xs
+    sizeLT n _ | n <= 0 = False
+    sizeLT n xs = f n xs where
+        f 0 _ = False
+        f _ [] = True
+        f n (_:xs) = f (n - 1) xs
 
 
 
@@ -56,4 +75,6 @@ instance (HasSize a,HasSize b) => HasSize (Either a b) where
 instance (HasSize a,HasSize b) => HasSize (a,b) where
     size (x,y) = size x + size y
 
+instance (HasSize a,HasSize b,HasSize c) => HasSize (a,b,c) where
+    size (x,y,z) = size x + size y  + size z
 
