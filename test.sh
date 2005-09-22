be3130acd1161885e402234105764013bc45d89d
John Meacham <john@repetae.net>
2005-09-22 14:26:04 +0900

    fix test script
    darcs-hash:20050922052604-1a7c6-bb9d19c99b9a0c2054cb7d9b6e603b3ae96aef82.gz

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ mkdir _darcs
 touch _darcs/inventory
 [ -e Doc ] || darcs get http://repetae.net/john/repos/Doc
 [ -e Boolean ] || darcs get http://repetae.net/john/repos/Boolean
-make && ./jhc -v /home/john/prog/hs/jhc/jhc/HelloWorld.hs
+make && ./jhc -v HelloWorld.hs
