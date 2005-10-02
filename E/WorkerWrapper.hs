6d4fc62bc540694692e0b510a214f7e5c94e6c36
John Meacham <john@repetae.net>
2005-10-02 21:07:59 +0900

    create  appropriate worker/wrappers for CPR analysis
    darcs-hash:20051002120759-1a7c6-4ddd01cf25178c447eef1188fc32b80b88e5dcd7.gz

--- E/WorkerWrapper.hs
+++ E/WorkerWrapper.hs
@@ -6,22 +6,26 @@ import DataConstructors
 import E.E
 import E.CPR
 import E.Strictness
+import E.Values
+import CanType
 import Info.Info as Info
 import Info.Types
 import Name
+import E.TypeCheck
 
 
 topLike Top = True
 topLike (Fun v) = topLike v
 topLike _ = False
 
-wrapable (Fun x) = f x where
-    f (Fun x) = f x
-    f (Tup _) = True
-    f _ = False
+wrapable (Fun x) (ELam _ e) = f x e where
+    f (Fun x) (ELam _ e) = f x e
+    f (Tup _) _ = True
+    f _ _ = False
+wrapable _ _ = False
 
 workWrap :: DataTable -> TVr -> E -> [(TVr,E)]
-workWrap dataTable tvr e | wrapable cpr = ans where
+workWrap dataTable tvr e | wrapable cpr e = ans where
     cpr = maybe Top id (Info.lookup (tvrInfo tvr))
     sa = maybe L id (Info.lookup (tvrInfo tvr))
     ans = [(setProperty prop_WRAPPER tvr,wrapper),(setProperty prop_WORKER tvr',worker)]
@@ -29,13 +33,23 @@ workWrap dataTable tvr e | wrapable cpr = ans where
     workerName x = case fromId x of
         Just y -> toId (toName Val ("W@",'f':show y))
         Nothing -> toId (toName Val ("W@",'f':show x))
-    wt = undefined
-    worker = undefined
-    wrapper = undefined
-    Just c = getConstructor cname dataTable
+    wt = typeInfer dataTable  worker
+    worker = foldr ELam body' args where
+        body' = eCase body [cb] Unknown
+        cb = Alt (LitCons cname vars bodyTyp) (if isSingleton then EVar sv else (ELit $ unboxedTuple (map EVar vars)))
+    wrapper = foldr ELam ne args where
+        ne | isSingleton = eStrictLet sv (foldl EAp (EVar tvr') (map EVar args))  (ELit $ LitCons cname [EVar sv] bodyTyp)
+           | otherwise = eCase (foldl EAp (EVar tvr') (map EVar args)) [ca] Unknown
+        ca = Alt (unboxedTuple vars) (ELit $ LitCons cname (map EVar vars) bodyTyp)
+    vars@(~[sv]) = [  tVr i t | t <- slotTypes dataTable cname bodyTyp | i <- [2,4..] ]
+    isSingleton = case vars of
+        [v] -> getType (getType v) == eHash
+        _ -> False
+    bodyTyp = typeInfer dataTable body
     (cname,args,body) = f cpr e []
     f (Fun x) (ELam a e) as = f x e (a:as)
-    f (Tup n) e as = (n,as,e)
+    f (Tup n) e as = (n,reverse as,e)
+    f x y z = error $ show (x,y,z)
 workWrap _dataTable tvr e = [(tvr,e)]
 
 
