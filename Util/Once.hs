fbdc8fbdb53ce02ae7caac5cc606b61d0dd00e30
John Meacham <john@repetae.net>
2005-10-13 18:48:52 +0900

    add OnceMap that runs an IO action once for each distinct argument it is passed.
    darcs-hash:20051013094852-1a7c6-5846e2c70751b4dc5f574d992ec2489fc6f5721c.gz

--- Util/Once.hs
+++ Util/Once.hs
@@ -1,8 +1,23 @@
 
 -- | a simple type that only lets an IO action happen once, caching its result.
 
-module Util.Once(Once, newOnce, runOnce, altOnce) where
+module Util.Once(
+    Once,
+    newOnce,
+    runOnce,
+    altOnce,
 
+    OnceMap,
+    newOnceMap,
+    runOnceMap,
+    altOnceMap,
+    onceMapToList,
+    onceMapKeys,
+    onceMapElems
+
+    ) where
+
+import qualified Data.Map as Map
 import Data.IORef
 import Data.Dynamic
 
@@ -39,4 +54,48 @@ altOnce (Once ref) first second = do
             first
 
 
+-- | run an IO action at most once for each distinct argument
+
+newtype OnceMap a b = OnceMap (IORef (Map.Map a b))
+    deriving(Typeable)
+
+
+newOnceMap :: Ord a => IO (OnceMap a b)
+newOnceMap = do
+    r <- newIORef Map.empty
+    return $ OnceMap r
+
+runOnceMap :: Ord a => OnceMap a b -> a -> IO b -> IO b
+runOnceMap (OnceMap r) x act = do
+    m <- readIORef r
+    case Map.lookup x m of
+        Just y -> return y
+        Nothing -> do
+            y <- act
+            modifyIORef r (Map.insert x y)
+            return y
+
+altOnceMap :: Ord a => OnceMap a () -> a -> IO b -> IO b -> IO b
+altOnceMap (OnceMap ref) x first after = do
+    m <- readIORef ref
+    case Map.member x m of
+        True -> after
+        False -> do
+            modifyIORef ref (Map.insert x ())
+            first
+
+onceMapToList :: OnceMap a b -> IO [(a,b)]
+onceMapToList (OnceMap ref) = do
+    m <- readIORef ref
+    return $ Map.toList m
+
+onceMapKeys :: OnceMap a b -> IO [a]
+onceMapKeys (OnceMap ref) = do
+    m <- readIORef ref
+    return $ Map.keys m
+
+onceMapElems :: OnceMap a b -> IO [b]
+onceMapElems (OnceMap ref) = do
+    m <- readIORef ref
+    return $ Map.elems m
 
