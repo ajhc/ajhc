b2fc94536059484b35719933a7ca815855f1722b
John Meacham <john@repetae.net>
2005-10-25 17:27:17 +0900

    modify compileloop some.
    darcs-hash:20051025082717-1a7c6-257b05a33f03fe59c149e2c0c6774d36ffa3fb02.gz

--- utils/compileloop.zsh
+++ utils/compileloop.zsh
@@ -1,12 +1,16 @@
 #!/bin/zsh
 set +x
+
+settitle() {
+    [[ -t 1 ]] || return
+    print -Pn "\e]2;$1\a"
+}
+
 SLEEPFILE=/tmp/compileloop.sleeping
-TIMES=`ls -lt $(cat depend.make | sed -e 's/^HSFILES=//')`
+# TIMES=`ls -lt $(cat depend.make | sed -e 's/^HSFILES=//')`
 echo 0 > $SLEEPFILE
 while true; do
-    TTIMES=`ls -lt $(cat depend.make | sed -e 's/^HSFILES=//')`
-    if [[ "$TIMES" !=  "$TTIMES" ]] { TIMES=$TTIMES; rm -f $SLEEPFILE; echo "changed"; { date ; make $*;  echo $? > $SLEEPFILE } |& tee -a compile.log } else {
-        sleep 1
-    }
+    #TTIMES=`ls -lt $(cat depend.make | sed -e 's/^HSFILES=//')`
+    if ( make -q $* )  { settitle "cl: `cat $SLEEPFILE`" ; sleep 1 } else {  settitle "cl: working"; rm -f $SLEEPFILE; echo "changed"; { date ; make $*;  echo $? > $SLEEPFILE } |& tee -a compile.log; sleep 3 }
 done
 
