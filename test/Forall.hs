8781dc98bc7787fc7b7d70a4d28982b7c62a5d7e
John Meacham <john@repetae.net>
2005-12-05 13:46:37 +0900

    Add test case file for various type system trickiness
    darcs-hash:20051205044637-1a7c6-15f97474bbabd7dd6d2918292e3f8fd345184acf.gz

---
+++ test/Forall.hs
@@ -0,0 +1,31 @@
+-- Test program for various uses of higher order polymorphism
+
+-- useful bits
+newtype Id x = Id x
+    deriving(Show)
+
+-- interesting haskell 98 types
+newtype Rec f = In (f (Rec f))
+newtype StateM m s a = STM (a -> m (a,s))
+newtype Bot = Bot Bot
+    deriving(Show)
+
+-- other stuff
+
+-- TODO this causes segfault when shown
+data Empty
+
+
+-- forall in type synonym
+type IdentityFunc = forall a . a -> a
+
+-- explicit forall
+id1 :: forall a . a -> a
+id1 x = x
+
+id2 :: a -> IdentityFunc
+id2 _ x = x
+
+
+main = do
+    putStrLn "Done."
