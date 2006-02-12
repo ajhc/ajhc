c69515fce1e6bdbc3ab607159885560a75183509
John Meacham <john@repetae.net>
2006-02-12 16:20:12 +0900

    add array monad to library
    darcs-hash:20060212072012-1a7c6-f6226d18d225ed0d1780f87c2e8a5eea2c3d5223.gz

--- lib/Jhc/Array.hs
+++ lib/Jhc/Array.hs
@@ -1,16 +1,27 @@
 module Jhc.Array where
 
 
+-- The internal array type
 data Array__ a
 
+-- the built-in array quasi-monad
+data AT a
 
 
--- | the number representing the size of the array must be less than or equal to the number of
--- elements in the list or bad stuff happens.
-foreign import primitive "unsafeNewArray__" :: Int -> [a] -> (Array__ a)
+
+-- none of these routines have run-time checks
+foreign import primitive "newAT__" :: Int -> AT a -> Array__ a
+foreign import primitive "writeAT__" :: Int -> a -> AT a
+foreign import primitive "seqAT__" :: AT a -> AT a -> AT a
+foreign import primitive "doneAT__" :: Int -> a -> AT a
 
 
+
+-- lookup a value in an array
 foreign import primitive "unsafeAt__" :: Array__ a -> Int -> a
-foreign import primitive "unsafeCopyArray__" :: Int -> [Either (Int,Int,Array__ a) a] -> Array__ a
 
+
+newArray :: [a] -> Array__ a
+newArray xs = newAT__ (length as) $ foldr assign doneAT (zip [0..] xs) where
+    assign (i,v) rs = writeAT__ i v `seqAT__` rs
 
