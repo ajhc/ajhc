8b778e276bc35a4071c1b5b612934baaf3f78d3e
John Meacham <john@repetae.net>
2006-02-18 22:02:17 +0900

    Add documentation and appropriate warnings to Jhc.Hole
    darcs-hash:20060218130217-1a7c6-8b4a2acd191e74e194357f6f5ff462431a0c638b.gz

--- lib/Jhc/Hole.hs
+++ lib/Jhc/Hole.hs
@@ -1,26 +1,31 @@
+-- | this module provides a once-updatable value that may be used in pure code.
+-- it is an _unchecked_ error to read a hole before it has been filled in.
+-- filling in a hole has the effect of 'seq'ing its value immediatly so lift it in
+-- a datatype if this is an issue.
+--
+-- this module should not be used unless you really know what you are doing.
+-- incorrect usage may result in memory corruption.
 
-module Jhc.Hole(Hole,newHole,fillHole,readHole) where
+module Jhc.Hole(Hole(),newHole,fillHole,readHole) where
 
 import Jhc.IO
 
 newtype Hole a = Hole a
 
-
-
 -- | unchecked error if readHole is evaled before fillHole has filled it in.
 readHole :: Hole a -> a
 readHole (Hole x) = x
 
-
-
-foreign import primitive newHole__  :: World__ -> (World__,Hole a)
-foreign import primitive fillHole__ :: Hole a -> a -> World__ ->World__
-
+-- | create a new hole containing a garbage value. must not be read until it has been filled.
 newHole :: IO (Hole a)
 newHole = IO $ \_ world -> case newHole__ world of
     (world',r) -> JustIO world' r
 
+-- | it is an unchecked error to fill in the same hole more than once.
 fillHole :: Hole a -> a -> IO ()
 fillHole r v = IO $ \_ world -> case fillHole__ r v world of
     world' -> JustIO world' ()
+
+foreign import primitive newHole__  :: World__ -> (World__,Hole a)
+foreign import primitive fillHole__ :: Hole a -> a -> World__ ->World__
 
