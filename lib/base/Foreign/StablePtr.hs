b9caf8947ad7a439e7512d67d1ef18d5e4314b9c
John Meacham <john@repetae.net>
2006-04-27 13:34:07 +0900

    implement Foreign.StablePtr
    darcs-hash:20060427043407-1a7c6-0d48042e3e89fca5970d5c0fb27e8c3c1f1f5be7.gz

--- lib/base/Foreign/StablePtr.hs
+++ lib/base/Foreign/StablePtr.hs
@@ -1,2 +1,40 @@
--- | Just a dummy skeleton, fixme.
-module Foreign.StablePtr where
+module Foreign.StablePtr(
+    StablePtr(),
+    castStablePtrToPtr,
+    castPtrToStablePtr,
+    newStablePtr,
+    deRefStablePtr,
+    freeStablePtr
+    ) where
+
+import Foreign.Ptr
+
+newtype StablePtr a = StablePtr (Ptr ())
+data PlaceHolder
+
+castPtrToStablePtr :: Ptr () -> StablePtr a
+castPtrToStablePtr p = StablePtr p
+
+castStablePtrToPtr :: StablePtr a -> Ptr ()
+castStablePtrToPtr (StablePtr p) = p
+
+
+freeStablePtr :: StablePtr a -> IO ()
+freeStablePtr _ = return ()
+
+newStablePtr :: a -> IO (StablePtr a)
+newStablePtr x = do
+    ptr <- ref_ptr (unsafeCoerce x)
+    return (StablePtr ptr)
+
+deRefStablePtr :: StablePtr a -> IO a
+deRefStablePtr (StablePtr x) = do
+    v <- deref_ptr x
+    return (unsafeCoerce v)
+
+
+
+foreign import primitive deref_ptr :: Ptr () -> IO PlaceHolder
+foreign import primitive ref_ptr   :: PlaceHolder -> IO (Ptr ())
+
+foreign import primitive unsafeCoerce :: a -> b
