0fee596ab884d2b6bbede7aeeca9f8bfcda6ee83
John Meacham <john@repetae.net>
2006-04-29 11:20:26 +0900

    add travCollect
    darcs-hash:20060429022026-1a7c6-c62013456da00cd6766a86cfdd441a20e5017a30.gz

--- Util/Gen.hs
+++ Util/Gen.hs
@@ -2,6 +2,8 @@
 -- | similar to GenUtil but can rely on non-haskell 98 features
 module Util.Gen where
 
+import Control.Monad.Writer
+import Control.Monad.Identity
 import Data.Monoid
 import List
 
@@ -9,3 +11,9 @@ mconcatMap f xs = mconcat (map f xs)
 mconcatInter x xs = mconcat (intersperse x xs)
 
 mconcatMapM f xs = mapM f xs >>= return . mconcat
+
+
+
+travCollect :: Monoid w => (forall m . Monad m => (a -> m a) -> a -> m a) -> (a -> w) -> a -> w
+travCollect fn col x = execWriter (fn (\x -> tell (col x) >> return x) x)
+
