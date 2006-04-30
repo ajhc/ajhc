d282a3969eb49b45157ebaef786a4806cd2e562c
John Meacham <john@repetae.net>
2006-04-30 10:32:17 +0900

    allow specification of headers for graph dot file
    darcs-hash:20060430013217-1a7c6-95fdc673c9e78ed5d9c90165dc4e0e2f2e1c2be9.gz

--- Util/Graphviz.hs
+++ Util/Graphviz.hs
@@ -20,8 +20,8 @@ i2d = fromInteger . toInteger
 
 -- | Format a graph for graphviz with reasonable defaults: title of \"fgl\",
 -- 8.5x11 pages, one page, landscape orientation
-graphviz' :: Graph g => g a b -> (a -> [(String,String)]) -> (b -> [(String,String)]) -> String
-graphviz' g fnode fedge = graphviz g "fgl" fnode fedge  (8.5,11.0) (1,1) Landscape
+graphviz' :: Graph g => g a b -> [(String,String)] -> (a -> [(String,String)]) -> (b -> [(String,String)]) -> String
+graphviz' g headers fnode fedge = graphviz g "fgl" headers fnode fedge  (8.5,11.0) (1,1) Landscape
 
 sq :: String -> String
 sq ('"':s) | last s == '"'  = init s
@@ -33,14 +33,16 @@ sq s = s
 
 sl :: [(String,String)] -> String
 sl [] = []
-sl a = " [" ++ foldr ($) "]" (intersperse (',':) (map f a)) where
-    f (x,y) = ((x ++ " = " ++  (show y)) ++)
+sl a = " [" ++ foldr ($) "]" (intersperse (',':) (map showEq a)) where
 
+showEq :: (String,String) -> String -> String
+showEq (x,y) = ((x ++ " = " ++  (show y)) ++)
 
 
 
 graphviz :: Graph g =>    g a b   -- ^ The graph to format
 			  -> String  -- ^ The title of the graph
+                          -> [(String,String)]
                           -> (a -> [(String,String)])
                           -> (b -> [(String,String)])
 			  -> (Double, Double)	-- ^ The size
@@ -53,7 +55,7 @@ graphviz :: Graph g =>    g a b   -- ^ The graph to format
 			  -> String
 
 
-graphviz g t fnode fedge (w, h) p@(pw', ph') o =
+graphviz g t headers fnode fedge (w, h) p@(pw', ph') o =
     let n = labNodes g
 	e = labEdges g
 	ns = concatMap sn n
@@ -64,11 +66,12 @@ graphviz g t fnode fedge (w, h) p@(pw', ph') o =
 	--gs = show ((w*(i2d pw))-m)++","++show ((h*(i2d ph))-m)
 	gs = sz (w*(i2d pw)) (h*(i2d ph))
     in "digraph "++sq t++" {\n"
-	    ++"\tmargin = \"0\"\n"
-	    ++"\tpage = \""++ps++"\"\n"
-	    ++"\tsize = \""++gs++"\"\n"
-	    ++o2s o
-	    ++"\tratio = \"fill\"\n"
+--	    ++"\tmargin = \"0\"\n"
+--	    ++"\tpage = \""++ps++"\"\n"
+--	    ++"\tsize = \""++gs++"\"\n"
+            ++ concatMap (\x -> showEq x "\n") headers
+	    ++ o2s o
+--	    ++"\tratio = \"fill\"\n"
 	    ++ns
 	    ++es
 	++"}"
