feab1f6a4f4e293d93e866fe98bf6a21072fb16e
John Meacham <john@repetae.net>
2006-10-12 17:09:08 +0900

    update for ghc 6.6
    darcs-hash:20061012080908-1a7c6-39353f969bb033e05899bc24212a13034537ab9b.gz

--- Util/TrueSet.hs
+++ Util/TrueSet.hs
@@ -9,7 +9,7 @@ module Util.TrueSet(
     delete,
     unions,
     union,
-    intersect,
+    intersection,
     intersects,
     difference,
     (\\)
@@ -38,21 +38,21 @@ delete x (TrueSet s False) = TrueSet (Set.delete x s) False
 delete x (TrueSet s True) = TrueSet (Set.insert x s) True
 
 unions xs = foldlStrict union empty xs
-intersects xs = foldlStrict intersect full xs
+intersects xs = foldlStrict intersection full xs
 
 foldlStrict f z xs
   = case xs of
       []     -> z
       (x:xx) -> let z' = f z x in seq z' (foldlStrict f z' xx)
 
-difference x y = x `intersect` invert y
+difference x y = x `intersection` invert y
 m1 \\ m2 = difference m1 m2
 
-(TrueSet x True)  `intersect` (TrueSet y True) = TrueSet (x `Set.union` y) True
-(TrueSet x False) `intersect` (TrueSet y False) = TrueSet (x `Set.intersect` y) False
-(TrueSet x True)  `intersect` (TrueSet y False) = TrueSet (y Set.\\ x) False
-(TrueSet x False) `intersect` (TrueSet y True) = TrueSet (x Set.\\ y) False
-(TrueSet x True)  `union` (TrueSet y True) = TrueSet (x `Set.intersect` y) True
+(TrueSet x True)  `intersection` (TrueSet y True) = TrueSet (x `Set.union` y) True
+(TrueSet x False) `intersection` (TrueSet y False) = TrueSet (x `Set.intersection` y) False
+(TrueSet x True)  `intersection` (TrueSet y False) = TrueSet (y Set.\\ x) False
+(TrueSet x False) `intersection` (TrueSet y True) = TrueSet (x Set.\\ y) False
+(TrueSet x True)  `union` (TrueSet y True) = TrueSet (x `Set.intersection` y) True
 (TrueSet x False) `union` (TrueSet y False) = TrueSet (x `Set.union` y) False
 (TrueSet x True)  `union` (TrueSet y False) = TrueSet (x Set.\\ y) True
 (TrueSet x False) `union` (TrueSet y True) = TrueSet (y Set.\\ x) True
