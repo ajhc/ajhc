bca29c8ebe1b8ef6479d6cc4d653aeed03dbdbd8
John Meacham <john@repetae.net>
2006-11-15 13:47:02 +0900

    make Jhc.Array compile, use unboxed values
    darcs-hash:20061115044702-1a7c6-31fa606b9931132ee0d8e3cfcd0126eaa34b782a.gz

--- lib/base/Jhc/Array.hs
+++ lib/base/Jhc/Array.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_JHC -N #-}
 module Jhc.Array(
     Array__(),
     AT(),
@@ -7,38 +8,56 @@ module Jhc.Array(
     writeAT__,
     unsafeAt__,
     newArray
+    )
     where
 
+import Jhc.Basics
 import Jhc.IO(dependingOn)
+import Jhc.Int
 
 -- The internal array type
-data Array__ a
+data Array__ :: * -> #
 
 -- the built-in array quasi-monad
 newtype AT a = AT (Array__ a -> Array__ a)
 
 seqAT__ :: AT a -> AT a -> AT a
-seqAT__ (AT a1) (AT a2) = AT $ \a -> a2 (a1 a)
+seqAT__ (AT a1) (AT a2) = AT $ \a -> case a1 a of
+    a' -> a2 a'
 
 doneAT__ :: AT a
-doneAT__ = AT id
+doneAT__ = AT (\arr -> arr)
 
 newAT__ :: Int -> AT a -> Array__ a
-newAT__ n (AT a1) = a1 (prim_newAT__ (n `dependingOn` a1))
+newAT__ n (AT a1) = case unboxInt (n `dependingOn` a1) of
+    nn -> case prim_newAT__ nn of a' -> a1 a'
 
 writeAT__ :: Int -> a -> AT a
-writeAT__ i x = AT $ \a -> prim_writeAT__ i x a
+writeAT__ i x = case unboxInt i of i' -> prim_writeAT__ i' x
 
 -- none of these routines have run-time checks
-foreign import primitive prim_newAT__ :: Int -> Array__ a
-foreign import primitive prim_writeAT__ :: Int -> a -> Array__ a -> Array__ a
+foreign import primitive prim_newAT__ :: Int__ -> Array__ a
+foreign import primitive prim_writeAT__ :: Int__ -> a -> AT a
+--foreign import primitive prim_copyAT__ :: Int__ -> Int__ -> Array__ a -> AT a
 
 -- lookup a value in an array
-foreign import primitive unsafeAt__ :: Array__ a -> Int -> a
+foreign import primitive unsafeAt__ :: Array__ a -> Int__ -> a
 
 
-newArray :: [a] -> Array__ a
-newArray xs = newAT__ (length xs) $ foldr assign doneAT__ (zip [0..] xs) where
-    assign (i,v) rs = writeAT__ i v `seqAT__` rs
+
+--newArray :: [a] -> Array__ a
+--newArray xs = newAT__ (length xs) $ foldr assign doneAT__ (zip [0..] xs) where
+--    assign (i,v) rs = writeAT__ i v `seqAT__` rs
+
+newArray :: Int -> [(Int,a)] -> Array__ a
+newArray n xs = newAT__ n (foldr assign doneAT__ xs) where
+        assign (i,v) rs = writeAT__ i v `seqAT__` rs
 
 
+{-
+newArray :: Int -> [a] -> Array__ a
+newArray n xs = case unboxInt n of
+    un -> newAT__ un $ f zero__ xs where
+        f _ [] = doneAT__
+        f n (x:xs) = case increment__ n of nn -> writeAT__ n x `seqAT__` f nn xs
+-}
