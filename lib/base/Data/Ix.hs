a1541905f8935b943d7b41e59be4f366b1fdd833
John Meacham <john@repetae.net>
2006-11-15 14:19:26 +0900

    make Data.Ix not depend on Prelude
    darcs-hash:20061115051926-1a7c6-e104da6bb51c8a8c384307fca8ea3a7487ec29ea.gz

--- lib/base/Data/Ix.hs
+++ lib/base/Data/Ix.hs
@@ -1,5 +1,11 @@
+{-# OPTIONS_JHC -N #-}
 module Data.Ix ( Ix(range, index, inRange, rangeSize) ) where
 
+import Jhc.Int
+import Jhc.Enum
+import Jhc.Order
+import Jhc.Basics
+import Jhc.IO
 
 class  Ord a => Ix a  where
     range     :: (a,a) -> [a]
@@ -7,8 +13,9 @@ class  Ord a => Ix a  where
     inRange   :: (a,a) -> a -> Bool
     rangeSize :: (a,a) -> Int
 
-    rangeSize b@(l,h) | null (range b) = 0
-                      | otherwise      = index b h + 1
+    rangeSize b@(l,h) = case range b of
+        [] -> zero
+        _  -> index b h `plus` one
 	-- NB: replacing "null (range b)" by  "not (l <= h)"
 	-- fails if the bounds are tuples.  For example,
 	-- 	(1,2) <= (2,1)
@@ -18,23 +25,26 @@ class  Ord a => Ix a  where
 instance  Ix Char  where
     range (m,n)		= [m..n]
     index b@(c,c') ci
-        | inRange b ci  =  fromEnum ci - fromEnum c
+        | inRange b ci  =  fromEnum ci `minus` fromEnum c
         | otherwise     =  error "Ix.index: Index out of range."
     inRange (c,c') i    =  c <= i && i <= c'
 
 instance  Ix Int  where
     range (m,n)		= [m..n]
     index b@(m,n) i
-        | inRange b i   =  i - m
+        | inRange b i   =  i `minus` m
         | otherwise     =  error "Ix.index: Index out of range."
     inRange (m,n) i     =  m <= i && i <= n
 
+{-
+XXX
 instance  Ix Integer  where
     range (m,n)		= [m..n]
     index b@(m,n) i
         | inRange b i   =  fromInteger (i - m)
         | otherwise     =  error "Ix.index: Index out of range."
     inRange (m,n) i     =  m <= i && i <= n
+-}
 
 -- instance (Ix a,Ix b) => Ix (a, b) -- as derived, for all tuples
 -- instance Ix Bool                  -- as derived
