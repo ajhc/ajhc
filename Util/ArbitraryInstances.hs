bfec0e9fad9c90607968502f1e266f2084f6991b
John Meacham <john@repetae.net>
2007-05-22 11:45:13 +0900

    remove unneeded arbitrary instancse
    darcs-hash:20070522024513-1a7c6-ffa710ff137d67b9ec551f91b1a65d90235eb880.gz

--- Util/ArbitraryInstances.hs
+++ Util/ArbitraryInstances.hs
@@ -4,20 +4,20 @@ import Test.QuickCheck
 import Monad
 import Char(chr)
 
-instance Arbitrary a => Arbitrary (Maybe a) where
-    arbitrary = do
-        i <- choose ((0::Int),7)
-        if i == 0 then return Nothing else do
-            x <- arbitrary
-            return (Just x)
+--instance Arbitrary a => Arbitrary (Maybe a) where
+--    arbitrary = do
+--        i <- choose ((0::Int),7)
+--        if i == 0 then return Nothing else do
+--            x <- arbitrary
+--            return (Just x)
     --coarbitrary Nothing = variant 0 . coarbitrary
-instance (Arbitrary a, Arbitrary b) => Arbitrary (Either a b) where
-    arbitrary = do
-        i <- choose ((0::Int),1)
-        case i of
-            0 -> liftM Left arbitrary
-            1 -> liftM Right arbitrary
-
+--instance (Arbitrary a, Arbitrary b) => Arbitrary (Either a b) where
+--    arbitrary = do
+--        i <- choose ((0::Int),1)
+--        case i of
+--            0 -> liftM Left arbitrary
+--            1 -> liftM Right arbitrary
+--
 instance Arbitrary Char where
     arbitrary = g where
         g = do
