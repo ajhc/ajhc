4538831963a283bea91ca748d1d5694345f2bd2b
John Meacham <john@repetae.net>
2008-02-09 14:31:00 +0900

    make 'rpm' build properly.
    darcs-hash:20080209053100-1a7c6-00b4d2fe43bce2a1b7d5eb321217fc5d903cf0d8.gz

--- utils/update_datestamp.sh
+++ utils/update_datestamp.sh
@@ -2,6 +2,10 @@
 CHANGELOG=$1
 CONFIGUREAC=$2
 
-DATESTAMP=`date -d "\`head $CHANGELOG | grep -o '^.*\d\d\d\d'\`" "+%Y%m%d"`
-echo "New Datestamp: $DATESTAMP"
+set -ex
+
+DATESTAMP=$(grep -o '^[A-Z].*[0-9][0-9][0-9][0-9]' "$CHANGELOG" | sort -n | head -1 |  xargs -n 1 '-d\n' date "+%Y%m%d" -d)
+SHORTVERSION=$(sed -ne 's/^AC_INIT(\[.*\],\[\(.*\)\.[0-9]*\])/\1/p' $CONFIGUREAC)
 sed -i -e 's/^\(AC_INIT(\[.*\],\[.*\.\)[0-9]*\(\])\)/\1'$DATESTAMP'\2/' $CONFIGUREAC
+sed -i -e 's/^REVISION=\([0-9]*\)$/REVISION='$DATESTAMP'/' $CONFIGUREAC
+sed -i -e 's/^SHORTVERSION=\([0-9.]*\)$/SHORTVERSION='$SHORTVERSION'/' $CONFIGUREAC
