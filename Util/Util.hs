4865409bced38ed38ee762eada3fb85cbfcc723b
John Meacham <john@repetae.net>
2008-02-11 00:26:19 +0900

    fix binary builds
    darcs-hash:20080210152619-1a7c6-635bef8b39403b6061465a57e91cebd52e69ba90.gz

---
+++ Util/Util.hs
@@ -0,0 +1,11 @@
+module Util.Util(
+    module Data.Monoid,
+    module Data.List,
+    module Data.Maybe,
+    module Control.Monad
+    ) where
+
+import Data.Monoid
+import Data.Maybe
+import Data.List
+import Control.Monad
