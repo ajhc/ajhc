c03787d71ea66b88992b77bdcb0eef2663ee9736
John Meacham <john@repetae.net>
2008-02-10 21:56:24 +0900

    update version to 0.4
    darcs-hash:20080210125624-1a7c6-110c98123fa78c82737e6fb9da0e3f8e3eb57f33.gz

--- utils/update_datestamp.sh
+++ utils/update_datestamp.sh
@@ -4,7 +4,7 @@ CONFIGUREAC=$2
 
 set -ex
 
-DATESTAMP=$(grep -o '^[A-Z].*[0-9][0-9][0-9][0-9]' "$CHANGELOG" | sort -n | head -1 |  xargs -n 1 '-d\n' date "+%Y%m%d" -d)
+DATESTAMP=$(grep -o '^[A-Z].*[0-9][0-9][0-9][0-9]' "$CHANGELOG" | sort -n | tail -1 |  xargs -n 1 '-d\n' date "+%Y%m%d" -d)
 SHORTVERSION=$(sed -ne 's/^AC_INIT(\[.*\],\[\(.*\)\.[0-9]*\])/\1/p' $CONFIGUREAC)
 sed -i -e 's/^\(AC_INIT(\[.*\],\[.*\.\)[0-9]*\(\])\)/\1'$DATESTAMP'\2/' $CONFIGUREAC
 sed -i -e 's/^REVISION=\([0-9]*\)$/REVISION='$DATESTAMP'/' $CONFIGUREAC
