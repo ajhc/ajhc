40f5b7bb0912990586367320ea33d897f02ce12e
John Meacham <john@repetae.net>
2008-02-21 12:21:34 +0900

    clean out old irrelevant code in Number.hs
    darcs-hash:20080221032134-1a7c6-8d7f99b040e9fbdc86d7052c97d84527f484137f.gz

--- Number.hs
+++ Number.hs
@@ -1,45 +1,7 @@
-module Number(Number(..),TypeInfo,toIntegral) where
+module Number(Number(..),toIntegral) where
 
 import Ratio
 import Data.Binary
-
-data NumType = Signed | Unsigned | Floating | Pointer
-data Value a = Known a | Unknown | AtLeast a | GreatestOfAll
-
-data TypeInfo = TypeInfo {
-    typeType :: NumType,
-    typeBytes :: Value Int
-    }
-
-{-
-signed v n = (toName RawType v,TypeInfo Signed (Known n))
-unsigned v n = (toName RawType v,TypeInfo Unsigned (Known n))
-typeTable = [
-    signed "int" 0 { typeBytes = AtLeast 4 },
-    unsigned "unsigned int" 0 { typeBytes = AtLeast 4 },
-    signed "int8_t" 1,
-    signed "int16_t" 2,
-    signed "int32_t" 4,
-    signed "int64_t" 8,
-    signed "intmax_t" 0 { typeBytes = GreatestOfAll },
-    signed "intptr_t" 0 { typeBytes = AtLeast 4 },
-    unsigned "uint8_t" 1,
-    unsigned "uint16_t" 2,
-    unsigned "uint32_t" 4,
-    unsigned "uint64_t" 8,
-    unsigned "uintmax_t" 0 { typeBytes = GreatestOfAll },
-    unsigned "uintptr_t" 0 { typeBytes = AtLeast 4 },
-    unsigned "wchar_t" 0   { typeBytes = AtLeast 4 },
-    signed "wint_t" 0   { typeBytes = AtLeast 4 },
-    unsigned "size_t" 0   { typeBytes = AtLeast 4 },
-    signed "ssize_t" 0   { typeBytes = AtLeast 4 },
-    (toName RawType "HsPtr", TypeInfo Pointer (AtLeast 4)),
-    (toName RawType "HsFunPtr", TypeInfo Pointer (AtLeast 4)),
-    unsigned "char" 1,
-    unsigned "short" 2 { typeBytes = AtLeast 2 }
-    ]
-
- -}
 
 newtype Number = Number Rational
     deriving(Num,Eq,Ord,Binary,Real,Fractional,RealFrac,Enum)
@@ -60,12 +22,4 @@ toIntegral :: (Integral i,Monad m) => Number -> m i
 toIntegral (Number r) = case denominator r of
     1 -> return $ fromInteger (numerator r)
     _ -> fail $ "toInteger: Number not integer " ++ show r
-
---instance Show Number where
-
---data Number = Number {
---    numberValue :: Ratio,
---    numberType :: Atom
---    }
-
 
