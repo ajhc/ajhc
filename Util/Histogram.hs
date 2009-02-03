eeca50dbdaf3556c5ca6ee6b1c22ec0262f8fe27
Samuel Bronson <naesten@gmail.com>
2009-02-04 08:12:44 +0900

    Add missing sigs in Util.Histogram
    darcs-hash:20090203231244-90c7a-1d60d1a6f0081b006fc1251eba10214091f9513b.gz

--- Util/Histogram.hs
+++ Util/Histogram.hs
@@ -29,22 +29,47 @@ instance Ord a => Monoid (Histogram a) where
     mappend (Histogram a) (Histogram b) = Histogram $ Map.unionWith (+) a b
 
 
+singleton :: a -> Histogram a
 singleton a = Histogram (Map.singleton a 1)
+
+insert :: Ord a => a -> Histogram a -> Histogram a
 insert a (Histogram m) = Histogram (Map.insertWith (+) a 1 m)
+
+find :: (Ord a) => a -> Histogram a -> Int
 find a (Histogram m) = Map.findWithDefault 0 a m
+
+toList :: Histogram a -> [(a, Int)]
 toList (Histogram m) = Map.toAscList m
+
+satisfy :: (Int -> Bool) -> Histogram a -> [a]
 satisfy f (Histogram m) = [ a | (a,i) <- Map.toAscList m, f i ]
+
+satisfyKey :: (Int -> Bool) -> Histogram a -> [(a,Int)]
 satisfyKey f (Histogram m) = [ (a,i) | (a,i) <- Map.toAscList m, f i ]
+
+filter :: Ord a => (a -> Int -> Bool) -> Histogram a -> Histogram a
 filter f (Histogram m) = Histogram (Map.filterWithKey f m)
+
+keys :: Histogram a -> [a]
 keys (Histogram m) = Map.keys m
+elems :: Histogram a -> [Int]
 elems (Histogram m) = Map.elems m
+
+map :: Ord b => (a -> b) -> Histogram a -> Histogram b
 map f (Histogram m) = Histogram $ Map.fromList [ (f k,i) | (k,i) <- Map.toList m ]
+
+mapM :: (Monad m, Ord b) => (a -> m b) -> Histogram a -> m (Histogram b)
 mapM f (Histogram m) = do
         ds <- sequence [ do f k >>= return . flip (,) i  | (k,i) <- Map.toList m ]
         return $ Histogram (Map.fromList ds)
+
+mapM_ :: (Monad m) => (a -> m b) -> Histogram a -> m ()
 mapM_ f (Histogram m) = sequence_ [ do f k >>= return . flip (,) i  | (k,i) <- Map.toList m ]
+
 fromList :: Ord a => [a] -> Histogram a
 fromList xs = foldr insert empty xs
+
+empty :: Histogram a
 empty = Histogram Map.empty
 
 union :: Ord a => Histogram a -> Histogram a -> Histogram a
