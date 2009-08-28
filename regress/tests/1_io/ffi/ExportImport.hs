2a670c76e363c3e0fdc470d2b957baa902eeeb75
John Meacham <john@repetae.net>
2009-08-28 21:26:04 +0900

    fix export-import test on 64 bit systems
    Ignore-this: 9a06a7310635fa6bc0ac98d24d2001cd
    
    darcs-hash:20090828122604-1a7c6-b8432a9749231fcb303ebea4a4a4d6353e70632e.gz

--- regress/tests/1_io/ffi/ExportImport.hs
+++ regress/tests/1_io/ffi/ExportImport.hs
@@ -6,13 +6,13 @@ import Jhc.Basics
 import Jhc.Monad
 import Jhc.Prim
 
-putChar :: Char__ -> IO ()
-putChar c = c_putwchar (charToInt c)
+putChar :: Char -> IO ()
+putChar c = c_putwchar (charToInt c) >> return ()
 
-foreign import primitive "U2U" charToInt :: Char__ -> Int
-foreign import ccall "stdio.h jhc_utf8_putchar" c_putwchar :: Int -> IO ()
+foreign import primitive "U2U" charToInt :: Char -> Int
+foreign import ccall "stdio.h jhc_utf8_putchar" c_putwchar :: Int -> IO Int
 
-foreign export ccall "myputc" putChar :: Char__ -> IO ()
+foreign export ccall "myputc" putChar :: Char -> IO ()
 foreign import ccall "&myputc" p_putc :: FunPtr (Char -> IO ())
 foreign import ccall "dynamic" callPutc :: FunPtr (Char -> IO ()) -> Char -> IO ()
 
