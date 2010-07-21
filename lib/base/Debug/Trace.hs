b11a41a289b74886d3fc50cf832a88a0b4f97b49
John Meacham <john@repetae.net>
2010-07-21 15:09:40 +0900

    add Debug.Trace to library
    Ignore-this: 246318179aeffa66b0ed617140bc94ae
    
    darcs-hash:20100721060940-1a7c6-a5a35e87daf7a888cd0cfda3abcfed46348b23a1.gz

---
+++ lib/base/Debug/Trace.hs
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Debug.Trace
+-- Copyright   :  (c) The University of Glasgow 2001
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- The 'trace' function.
+--
+-----------------------------------------------------------------------------
+
+module Debug.Trace (
+        -- * Tracing
+        putTraceMsg,      -- :: String -> IO ()
+        trace,            -- :: String -> a -> a
+        traceShow
+  ) where
+
+import Prelude
+import System.IO.Unsafe
+import System.IO (hPutStrLn,stderr)
+
+-- | 'putTraceMsg' function outputs the trace message from IO monad.
+-- Usually the output stream is 'System.IO.stderr' but if the function is called
+-- from Windows GUI application then the output will be directed to the Windows
+-- debug console.
+putTraceMsg :: String -> IO ()
+putTraceMsg msg = do
+    hPutStrLn stderr msg
+
+{-# NOINLINE trace #-}
+{-|
+When called, 'trace' outputs the string in its first argument, before
+returning the second argument as its result. The 'trace' function is not
+referentially transparent, and should only be used for debugging, or for
+monitoring execution. Some implementations of 'trace' may decorate the string
+that\'s output to indicate that you\'re tracing. The function is implemented on
+top of 'putTraceMsg'.
+-}
+trace :: String -> a -> a
+trace string expr = unsafePerformIO $ do
+    putTraceMsg string
+    return expr
+
+{-|
+Like 'trace', but uses 'show' on the argument to convert it to a 'String'.
+
+> traceShow = trace . show
+-}
+traceShow :: (Show a) => a -> b -> b
+traceShow = trace . show
