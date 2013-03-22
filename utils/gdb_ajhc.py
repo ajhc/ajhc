a572ddeb55dc28124c4c2fba09330e8b97a6dce5
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-03-22 22:49:15 +0900

    Do not use gdb.parse_and_eval in loop. It's very slow.

--- utils/gdb_ajhc.py
+++ utils/gdb_ajhc.py
@@ -1,10 +1,8 @@
 import gdb
 
 def _gpe(arg):
+    """TOO SLOW!!!"""
     return gdb.parse_and_eval(arg)
-
-def _gv_null():
-    return _gpe('(void *) 0')
 
 class PrintHsHeapCommand (gdb.Command):
     """Dump Haskell heap."""
@@ -23,29 +21,26 @@ class PrintHsHeapCommand (gdb.Command):
     def _p_free_blocks(self):
         dict_normal = {}
         dict_mono = {}
-        s_block = str(_gpe('arena->free_blocks.slh_first'))
-        while int(s_block, 16) != 0:
-            flags = _gpe('((struct s_block *)' + s_block + ')->flags')
+        s_block = _gpe('arena->free_blocks.slh_first')
+        while int(str(s_block), 16) != 0:
+            s_block_dref = s_block.dereference()
+            flags = s_block_dref['flags']
             if flags & 16 == 0:
-                num_ptrs = _gpe('((struct s_block *)' + \
-                                    s_block + ')->u.pi.num_ptrs')
-                size = _gpe('((struct s_block *)' + s_block + ')->u.pi.size')
+                num_ptrs = s_block_dref['u']['pi']['num_ptrs']
+                size = s_block_dref['u']['pi']['size']
                 key = (int(num_ptrs), int(size))
                 if key not in dict_normal:
                     dict_normal[(key)] = 1
                 else:
                     dict_normal[(key)] += 1
             else:
-                num_ptrs = _gpe('((struct s_block *)' + \
-                                    s_block + ')->u.m.num_ptrs')
+                num_ptrs = s_block_dref['u']['m']['num_ptrs']
                 key = int(num_ptrs)
                 if key not in dict_mono:
                     dict_mono[(key)] = 1
                 else:
                     dict_mono[(key)] += 1
-            s_block = str(_gpe('((struct s_block *)' + \
-                                   s_block + \
-                                   ')->link.sle_next'))
+            s_block = s_block_dref['link']['sle_next']
         print "free normal blocks:"
         for k, v in dict_normal.iteritems():
             print ' ', k, ':',  v
