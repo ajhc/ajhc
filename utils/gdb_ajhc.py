c8d53949450b38555c38ce93ebd6d8003d334256
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-03-26 00:25:57 +0900

    Dump s_cache on gdb.

--- utils/gdb_ajhc.py
+++ utils/gdb_ajhc.py
@@ -14,9 +14,9 @@ class PrintJgcheapCommand (gdb.Command):
         block_used = _gpe('arena->block_used')
         block_threshold = _gpe('arena->block_threshold')
         number_gcs = _gpe('arena->number_gcs')
-        print 'block_used :     ', int(block_used)
-        print 'block_threshold :', int(block_threshold)
-        print 'number_gcs :     ', int(number_gcs)
+        print 'block_used:     ', int(block_used)
+        print 'block_threshold:', int(block_threshold)
+        print 'number_gcs:     ', int(number_gcs)
 
     def _p_free_blocks(self):
         dict_normal = {}
@@ -41,17 +41,17 @@ class PrintJgcheapCommand (gdb.Command):
                 else:
                     dict_mono[key] += 1
             s_block = s_block_dref['link']['sle_next']
-        print "free normal blocks :"
+        print "free normal blocks:"
         for k, v in dict_normal.iteritems():
             print ' ', k, ':',  v
-        print "free monolithic blocks :"
+        print "free monolithic blocks:"
         for k, v in dict_mono.iteritems():
             print ' ', k, ':', v
 
     def _p_s_cache(self):
         list_cache = []
         s_cache = _gpe('arena->caches.slh_first')
-        print "list s_cache :"
+        print "s_cache list:"
         while int(str(s_cache), 16) != 0:
             s_cache_dref = s_cache.dereference()
             num_ptrs = s_cache_dref['num_ptrs']
@@ -60,21 +60,42 @@ class PrintJgcheapCommand (gdb.Command):
             num_entries = s_cache_dref['num_entries']
 
             # collect blocks used partially
+            # blocks: num_free > 0
             blocks = s_cache_dref['blocks']['slh_first']
+            num_blocks = 0
+            num_free = 0
             while int(str(blocks), 16) != 0:
                 blocks_dref = blocks.dereference()
+                num_blocks += 1
+                num_free += int(blocks_dref['u']['pi']['num_free'])
                 blocks = blocks_dref['link']['sle_next']
 
             # collect blocks used fully
+            # full_blocks: num_free == 0
             full_blocks = s_cache_dref['full_blocks']['slh_first']
+            num_full_blocks = 0
+            num_free_full = 0
             while int(str(full_blocks), 16) != 0:
                 full_blocks_dref = full_blocks.dereference()
+                num_full_blocks += 1
+                num_free_full += int(blocks_dref['u']['pi']['num_free'])
                 full_blocks = full_blocks_dref['link']['sle_next']
 
-            list_cache.append((key, {'num_entries' : int(num_entries)}))
+            list_cache.append(
+                (key, {'num_entries': int(num_entries), \
+                           'blocks': {'num': num_blocks, \
+                                      'num_free': num_free}, \
+                           'full_blocks': {'num': num_full_blocks, \
+                                           'num_free': num_free_full}}))
             s_cache = s_cache_dref['next']['sle_next']
+
+        print "                       |         blocks         |       full_blocks"
+        print "           num_entries | num_blocks free_blocks | num_blocks free_blocks"
         for k, v in sorted(list_cache):
-            print ' ', k, ':', v
+            vp = '%011s %012s %011s %012s %011s' % \
+                (v['num_entries'], v['blocks']['num'], v['blocks']['num_free'], \
+                     v['full_blocks']['num'], v['full_blocks']['num_free'])
+            print ' ', k, ':', vp
 
     def invoke (self, arg, from_tty):
         self._p_arena()
