078461ca2dac8b8334f568f7197afc42eab0cd41
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-05-22 20:23:24 +0900

    SelfTest uses test-framework-th library.

--- selftest/SelfTest.hs
+++ selftest/SelfTest.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
 import Data.Monoid
 import Data.List(sort,nub)
 import Control.Monad
 import System.IO
+import Test.Framework.TH
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
+import Test.HUnit
 import qualified Data.Set as Set
 import qualified Data.List
 
@@ -26,40 +31,25 @@ import qualified Info.Info as Info
 
 {-# NOINLINE main #-}
 main :: IO ()
-main = do
-    testAtom
-    testProperties
-    testPackedString
-    testHasSize
-    testName
-    testInfo
-    testBinary
+main = $(defaultMainGenerator)
+-- XXXXXXXXXXXXX Should run testInfo and testBinary!!!
 
-qc s p = quickCheck $ label s p
-
-testAtom = do
-    let prop_atomneq xs ys = (xs /= ys) == (a1 /= a2) where
-            a1 = toAtom xs
-            a2 = toAtom (ys :: String)
-        prop_atomIndex (xs :: String) = intToAtom (fromAtom a) == Just a where
-            a = toAtom xs
-        prop_atomneq' xs ys = (xs `compare` ys) == (fromAtom a1 `compare` (fromAtom a2 :: BS.ByteString)) where
-            a1 = toAtom xs
-            a2 = toAtom (ys :: String)
-        prop_atomint xs = an > 0 && odd an where
-            an = fromAtom $ toAtom (xs :: String) :: Int
-        prop_atomii xs = Just xs == fromAtom `fmap` (intToAtom an) where
-            an = fromAtom $ toAtom (xs :: String) :: Int
-        prop_aappend (xs,ys) = (toAtom xs `mappend` toAtom ys) == toAtom ((xs::String) ++ ys)
-        prop_aappend' (xs,ys) = fromAtom (toAtom xs `mappend` toAtom ys) == ((xs::String) ++ ys)
-    qc "atom.id" $ \xs -> fromAtom (toAtom xs) == (xs::String)
-    qc "atom.eq" $ \xs -> (toAtom xs) == toAtom (xs::String)
-    qc "atom.index" prop_atomIndex
-    qc "atom.neq" prop_atomneq
-    qc "atom.eq'" prop_atomneq'
-    qc "atom.int" prop_atomint
-    qc "atom.ii" prop_atomii
-    qc "atom.aapend" prop_aappend
+prop_atom_id xs = fromAtom (toAtom xs) == (xs::String)
+prop_atom_eq xs = (toAtom xs) == toAtom (xs::String)
+prop_atom_index (xs :: String) = intToAtom (fromAtom a) == Just a where
+  a = toAtom xs
+prop_atom_neq xs ys = (xs /= ys) == (a1 /= a2) where
+  a1 = toAtom xs
+  a2 = toAtom (ys :: String)
+prop_atom_neq' xs ys = (xs `compare` ys) == (fromAtom a1 `compare` (fromAtom a2 :: BS.ByteString)) where
+  a1 = toAtom xs
+  a2 = toAtom (ys :: String)
+prop_atom_int xs = an > 0 && odd an where
+  an = fromAtom $ toAtom (xs :: String) :: Int
+prop_atom_ii xs = Just xs == fromAtom `fmap` (intToAtom an) where
+  an = fromAtom $ toAtom (xs :: String) :: Int
+prop_atom_aappend (xs,ys) = (toAtom xs `mappend` toAtom ys) == toAtom ((xs::String) ++ ys)
+prop_atom_aappend' (xs,ys) = fromAtom (toAtom xs `mappend` toAtom ys) == ((xs::String) ++ ys) -- xxx need?
 
 --strings = [ "foo", "foobar", "baz", "", "bob"]
 strings =  ["h","n\206^um\198(","\186","yOw\246$\187x#",";\221x<n","\201\209\236\213J\244\233","\189eW\176v\175\209"]
@@ -67,16 +57,11 @@ strings =  ["h","n\206^um\198(","\186","yOw\246$\187x#",";\221x<n","\201\209\236
 appendPS :: PackedString -> PackedString -> PackedString
 appendPS = mappend
 
-testPackedString = do
-    let prop_psid xs = unpackPS (packString xs) == (xs::String)
+prop_PackedString_psid xs = unpackPS (packString xs) == (xs::String)
 --        prop_pslen xs = lengthPS (packString xs) == length (xs::String)
-        prop_psappend (xs,ys) = (packString xs `appendPS` packString ys) == packString ((xs::String) ++ ys)
-        prop_psappend' (xs,ys) = unpackPS (packString xs `appendPS` packString ys) == ((xs::String) ++ ys)
-        prop_sort xs = sort (map packString xs) == map packString (sort xs)
-    qc "PackedString.psid" prop_psid
-    qc "PackedString.sort"  prop_sort
-    qc "PackedString.psappend"  prop_psappend
-    qc "PackedString.psappend'"  prop_psappend'
+prop_PackedString_sort xs = sort (map packString xs) == map packString (sort xs)
+prop_PackedString_psappend (xs,ys) = (packString xs `appendPS` packString ys) == packString ((xs::String) ++ ys)
+prop_PackedString_psappend' (xs,ys) = unpackPS (packString xs `appendPS` packString ys) == ((xs::String) ++ ys)
 --    quickCheck prop_pslen
 --    print $ map packString strings
 --    print $ sort $ map packString strings
@@ -90,45 +75,36 @@ testPackedString = do
 
 --pshash xs = putStrLn $ xs ++ ": " ++ show (hashPS (packString xs ))
 
-testName = do
-    let nn x = (not (null x)) && ';' `notElem` x
-        prop_acc t a b = nn a && nn b ==> let
-            n = toName t (a::String,b::String)
-            un = toUnqualified n
-            in  nameType n == t && getModule n == Just (toModule a) && getModule un == Nothing && show n == (a ++ "." ++ b) && case t of
-              QuotedName -> show un == '`':b
-              _          -> show un == b
-    qc "name.tofrom" $ \t a b -> nn a && nn b ==> fromName (toName t (a::String,b::String)) == (t,(a,b))
-    qc "name.nameparts" $ \t a b -> nn b ==> nameParts (toName t (a,b)) == (t,a,b)
-    qc "name.nameparts2" $ \t a b ->  nn b ==> nameParts (toName t (a,b)) == (t,Just a,b)
-    qc "name.toUnqualified" $ \t a b -> nn a && nn b ==> toUnqualified (toName t (a,b)) == toName t b
-    qc "name.getModule" $ \ t a b -> nn a && nn b ==> getModule (toName t (a,b)) == Just (toModule a)
-    qc "name.getModule2" $ \ t b -> nn b ==> getModule (toName t b) == Nothing
-    qc "name.getIdent" $ \t a b ->  nn b ==> getIdent (toName t (a :: Maybe Module,b)) == b
-    qc "name.setModule" $ \t a b c ->  nn b && nn c ==> setModule (toModule c) (toName t (a :: Maybe Module,b)) == toName t (c,b)
-    qc "name.show" $ \ t a b -> nn a && nn b ==> show (toName t (a,b)) == a ++ "." ++ b
-    qc "name.quote" $ \ t a b -> nn a && nn b ==> let n = toName t (a,b) in fromQuotedName (quoteName n) == Just n
-    qc "name.noquote" $ \ t a b -> t /= QuotedName && nn b ==> collect t $ fromQuotedName (toName t (a :: Maybe Module,b)) == Nothing
-    qc "name.acc" prop_acc
-    qc "name.tup" $ \n -> n >= 0 ==> fromUnboxedNameTuple (unboxedNameTuple RawType n) == Just n
-    qc "name.overlap" $ \t -> (isTypeNamespace t,isValNamespace t) /= (True,True)
+nn x = (not (null x)) && ';' `notElem` x
 
-testProperties = do
-    let prop_list x xs = sort (Data.List.delete x $ nub xs) == toList p where
-            p = unsetProperty x ((fromList xs) :: Properties)
-        prop_enum :: Info.Types.Property -> Info.Types.Property -> Bool
-        prop_enum x y = (fromEnum x `compare` fromEnum y) == (x `compare` y)
-    qc "prop.list" prop_list
-    qc "prop.enum" prop_enum
+prop_name_tofrom t a b = nn a && nn b ==> fromName (toName t (a::String,b::String)) == (t,(a,b))
+prop_name_nameparts t a b = nn b ==> nameParts (toName t (a,b)) == (t,a,b)
+prop_name_nameparts2 t a b =  nn b ==> nameParts (toName t (a,b)) == (t,Just a,b)
+prop_name_toUnqualified t a b = nn a && nn b ==> toUnqualified (toName t (a,b)) == toName t b
+prop_name_getModule  t a b = nn a && nn b ==> getModule (toName t (a,b)) == Just (toModule a)
+prop_name_getModule2  t b = nn b ==> getModule (toName t b) == Nothing
+prop_name_getIdent t a b =  nn b ==> getIdent (toName t (a :: Maybe Module,b)) == b
+prop_name_setModule t a b c =  nn b && nn c ==> setModule (toModule c) (toName t (a :: Maybe Module,b)) == toName t (c,b)
+prop_name_show  t a b = nn a && nn b ==> show (toName t (a,b)) == a ++ "." ++ b
+prop_name_quote  t a b = nn a && nn b ==> let n = toName t (a,b) in fromQuotedName (quoteName n) == Just n
+prop_name_noquote  t a b = t /= QuotedName && nn b ==> collect t $ fromQuotedName (toName t (a :: Maybe Module,b)) == Nothing
+prop_name_acc t a b = nn a && nn b ==> let
+  n = toName t (a::String,b::String)
+  un = toUnqualified n
+  in  nameType n == t && getModule n == Just (toModule a) && getModule un == Nothing && show n == (a ++ "." ++ b) && case t of
+    QuotedName -> show un == '`':b
+    _          -> show un == b
+prop_name_tup n = n >= 0 ==> fromUnboxedNameTuple (unboxedNameTuple RawType n) == Just n
+prop_name_overlap t = (isTypeNamespace t,isValNamespace t) /= (True,True)
 
-testHasSize = do
-    let prop_gt (xs,n) = sizeGT n (xs::[Int]) == (length xs > n)
-        prop_gte (xs,n) = sizeGTE n (xs::[Int]) == (length xs >= n)
-        prop_lte (xs,n) = sizeLTE n (xs::[Int]) == (length xs <= n)
-    qc "hasSize.gt" prop_gt
-    qc "hasSize.gte" prop_gte
-    qc "hasSize.lte" prop_lte
+prop_prop_list x xs = sort (Data.List.delete x $ nub xs) == toList p where
+    p = unsetProperty x ((fromList xs) :: Properties)
+prop_prop_enum :: Info.Types.Property -> Info.Types.Property -> Bool
+prop_prop_enum x y = (fromEnum x `compare` fromEnum y) == (x `compare` y)
 
+prop_hasSize_gt (xs,n) = sizeGT n (xs::[Int]) == (length xs > n)
+prop_hasSize_gte (xs,n) = sizeGTE n (xs::[Int]) == (length xs >= n)
+prop_hasSize_lte (xs,n) = sizeLTE n (xs::[Int]) == (length xs <= n)
 
 testInfo = do
     putStrLn "Testing Info"
