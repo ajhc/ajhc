73cedc4842866f48590ce191b3f79ea8392aba16
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-06-05 16:31:54 +0900

    Assign FunPtr as CTYPE "HsFunPtr".

--- lib/jhc/Jhc/Type/Ptr.hs
+++ lib/jhc/Jhc/Type/Ptr.hs
@@ -3,4 +3,4 @@ module Jhc.Type.Ptr where
 import Jhc.Prim.Bits
 
 data {-# CTYPE "HsPtr" #-} Ptr a = Ptr Addr_
-data {-# CTYPE "FunPtr" #-} FunPtr a = FunPtr FunAddr_
+data {-# CTYPE "HsFunPtr" #-} FunPtr a = FunPtr FunAddr_
