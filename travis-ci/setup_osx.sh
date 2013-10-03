b76ed5ab6fa963069c700e9c63d15b08c32dbbfd
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-10-04 02:57:15 +0900

    Install YAML.pm and valgrind with homebrew.

--- travis-ci/setup_osx.sh
+++ travis-ci/setup_osx.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 wget "http://lambda.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
 sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
+brew update
+brew install perl valgrind
 cabal update || cabal update
 export HSPACKS="derive temporary haskeline utf8-string HsSyck cpphs test-framework-th test-framework-hunit test-framework-quickcheck2 uniplate"
 export HSOPTS="--disable-documentation --disable-library-profiling --disable-executable-profiling"
