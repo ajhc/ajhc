e925c9f217eaa0d078fe5855bcaf06272b35c17a
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-10-04 04:35:41 +0900

    Nedd to call sudo for cpan?

--- travis-ci/setup_osx.sh
+++ travis-ci/setup_osx.sh
@@ -3,7 +3,7 @@ wget "http://lambda.haskell.org/platform/download/2013.2.0.0/Haskell Platform 20
 sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
 brew update
 brew install valgrind
-echo "yes" | cpan install YAML
+echo "yes" | sudo cpan install YAML
 cabal update || cabal update
 export HSPACKS="derive temporary haskeline utf8-string HsSyck cpphs test-framework-th test-framework-hunit test-framework-quickcheck2 uniplate"
 export HSOPTS="--disable-documentation --disable-library-profiling --disable-executable-profiling"
