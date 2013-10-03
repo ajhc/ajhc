0b1944d7d61d2408c502b0cf92db7715c8f4f569
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-10-04 00:14:10 +0900

    Do not run test on travis-ci + Mac OS X.

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -2,5 +2,5 @@
 wget "http://lambda.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
 sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
 cabal update || cabal update
-export HSPACKS="derive temporary haskeline utf8-string HsSyck test-framework-th test-framework-hunit test-framework-quickcheck2 uniplate"
+export HSPACKS="derive temporary haskeline utf8-string HsSyck cpphs"
 cabal install $HSPACKS || cabal install $HSPACKS
