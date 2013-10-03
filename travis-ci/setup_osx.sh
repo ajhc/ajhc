e7ec81ec9e8c9131755820783a081fd935eadcb3
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-10-04 03:13:56 +0900

    Install YAML.pm with CPAN.

--- travis-ci/setup_osx.sh
+++ travis-ci/setup_osx.sh
@@ -2,7 +2,8 @@
 wget "http://lambda.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
 sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
 brew update
-brew install perl valgrind
+brew install valgrind
+cpan install YAML
 cabal update || cabal update
 export HSPACKS="derive temporary haskeline utf8-string HsSyck cpphs test-framework-th test-framework-hunit test-framework-quickcheck2 uniplate"
 export HSOPTS="--disable-documentation --disable-library-profiling --disable-executable-profiling"
