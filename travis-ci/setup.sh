0dc1e21272052883afe4b7dcd2a55750341a3753
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-10-03 19:29:22 +0900

    Split travis-ci setting for Mac OS X.

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-sudo apt-get -qq update
-sudo apt-get -qq -y install make locales autoconf m4 libconfig-yaml-perl graphviz haskell-platform cpphs pandoc hscolour po4a libgc-dev gcc-multilib valgrind
-sudo apt-get -qq -y install libghc-temporary-dev libghc-haskeline-dev libghc-utf8-string-dev libghc-hssyck-dev libghc-test-framework-th-dev libghc-test-framework-hunit-dev libghc-test-framework-quickcheck2-dev libghc-uniplate-dev
+wget "http://lambda.haskell.org/platform/download/2013.2.0.0/Haskell Platform 2013.2.0.0 32bit.pkg"
+sudo installer -pkg "Haskell Platform 2013.2.0.0 32bit.pkg" -target /
 cabal update || cabal update
-cabal install derive-2.5.11 || cabal install derive-2.5.11
+export HSPACKS="derive temporary haskeline utf8-string HsSyck test-framework-th test-framework-hunit test-framework-quickcheck2 uniplate"
+cabal install $HSPACKS || cabal install $HSPACKS
