8c13cc98aa056e54e75670a91aecb8a2a093845c
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-11-06 19:08:57 +0900

    Better test code for issue/51.

--- regress/tests/6_fixed_bugs/UsedPointerSweeped/UsedPointerSweeped.c
+++ regress/tests/6_fixed_bugs/UsedPointerSweeped/UsedPointerSweeped.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+static int count = 0;
+
 // return costant value
 int get_int(void) {  return 42; }
 
 // return constant value
-const char* get_str(void) {
-  return "So Long, and Thanks for All the Fish";
+const char *get_str(void) {
+	return "So Long, and Thanks for All the Fish";
 }
 
-void* make_entry(void) {
-  /* allocate some pointer */
-  return (void*)0xdeadbeaf;
+void *make_entry(void) {
+	/* allocate some pointer */
+	return (void *)0xdeadbeaf;
 }
 
 void dump(void* p) {
-  if(p != 0xdeadbeaf) {
-    printf("wow! someone overwrite my pointer!(%x)\n", p);
-    abort();
-  }
+	uint32_t p1 = (uint32_t) make_entry();
+	uint32_t p2 = (uint32_t) p;
+
+	if (count++ > 1000) {
+		exit(0);
+	}
+	if (p1 != p2) {
+		printf("wow! someone overwrite my pointer!(0x%x != 0x%x)\n", p1, p2);
+		abort();
+	}
 }
