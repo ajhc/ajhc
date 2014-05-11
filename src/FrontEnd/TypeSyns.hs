dec2c975b9ec50a7e5ed19ce9cf8a8c55d143a84
John Meacham <john@repetae.net>
2014-05-11 22:07:27 +0900

    remove more renaming cruft from TypeSyns
    Ignore-this: a2e8427e8412aabdc1b67dc7501f9173
    
    darcs-hash:20140511130727-1a7c6-9b842c5325a286c61d10b16132baf7cb8abaae18.gz

--- src/FrontEnd/TypeSyns.hs
+++ src/FrontEnd/TypeSyns.hs
@@ -11,14 +11,12 @@ import FrontEnd.Syn.Traverse
 import FrontEnd.TypeSynonyms
 import FrontEnd.Warning
 
-type SubTable = ()
-
 -- the monadic state
 
 data ScopeState = ScopeState {
-    errors         :: [Warning],
-    synonyms       :: TypeSynonyms,
-    srcLoc         :: !SrcLoc
+    errors   :: [Warning],
+    synonyms :: TypeSynonyms,
+    srcLoc   :: !SrcLoc
     }
 
 -- The monadic type
@@ -35,40 +33,11 @@ instance MonadSetSrcLoc ScopeSM where
 expandTypeSyns :: (Expand x,MonadWarn m) => TypeSynonyms -> x -> m x
 expandTypeSyns syns m = ans where
     startState = ScopeState {
-        errors         = [],
-        synonyms       =  syns,
-        srcLoc         = bogusASrcLoc
-        }
+        errors   = [],
+        synonyms = syns,
+        srcLoc   = bogusASrcLoc }
     (rm, fs) = runState (expand m) startState
-    ans = do
-        mapM_ addWarning (errors fs)
-        return rm
-
-{-
-expandTypeSyns :: MonadWarn m => TypeSynonyms -> HsModule -> m HsModule
-expandTypeSyns syns m = ans where
-    startState = ScopeState {
-        errors         = [],
-        synonyms       =  syns,
-        srcLoc         = bogusASrcLoc
-        }
-    (rm, fs) = runState (expand m) startState
-    ans = do
-        mapM_ addWarning (errors fs)
-        return rm
-
-expandTypeSynsStmt :: MonadWarn m => TypeSynonyms -> Module -> HsStmt -> m HsStmt
-expandTypeSynsStmt syns _ m = ans where
-    startState = ScopeState {
-        errors         = [],
-        synonyms       =  syns,
-        srcLoc         = bogusASrcLoc
-        }
-    (rm, fs) = runState (renameHsStmt m ()) startState
-    ans = do
-        mapM_ addWarning (errors fs)
-        return rm
-        -}
+    ans = mapM_ addWarning (errors fs) >> return rm
 
 class Expand a where
     expand :: a -> ScopeSM a
@@ -84,204 +53,137 @@ instance Expand HsModule where
         return tidy { hsModuleDecls = decls' }
 
 instance Expand HsDecl where
-    expand x = renameHsDecl x ()
     expandList decls = do
         let expandTypeSigs ds =  (concatMap f ds) where
             f (HsTypeSig sl ns qt) =  [ HsTypeSig sl [n] qt | n <- ns]
             f d = [d]
         mapM expand (expandTypeSigs decls)
+    expand x = f x where
+        f (HsPatBind srcLoc hsPat hsRhs {-where-} hsDecls) = withSrcLoc srcLoc $ do
+            hsPat'    <- expand hsPat
+            hsDecls'  <- expand hsDecls
+            hsRhs'    <- expand hsRhs
+            return (HsPatBind srcLoc hsPat' hsRhs' {-where-} hsDecls')
+        f (HsForeignDecl a b n t) = withSrcLoc a $ do
+            t <- expand t
+            return  (HsForeignDecl a b n t)
+        f (HsForeignExport a b n t) = withSrcLoc a $ do
+            t <- expand t
+            return  (HsForeignExport a b n t)
+        f (HsFunBind hsMatches) = HsFunBind <$> expand hsMatches
+        f (HsTypeSig srcLoc hsNames hsQualType) = withSrcLoc srcLoc $ do
+            hsQualType' <- expand hsQualType
+            return (HsTypeSig srcLoc hsNames hsQualType')
+        f dl@HsDataDecl { hsDeclContext = hsContext, hsDeclCons = hsConDecls  } = do
+            hsContext' <- expand hsContext
+            hsConDecls' <- expand hsConDecls
+            return dl { hsDeclContext = hsContext', hsDeclCons = hsConDecls' }
+        f (HsTypeDecl srcLoc name hsNames t) = withSrcLoc srcLoc $ do
+            t' <- renameHsType' False t
+            return (HsTypeDecl srcLoc name hsNames t')
+        f decl@HsActionDecl { hsDeclSrcLoc = srcLoc, hsDeclExp = e }  = withSrcLoc srcLoc $ do
+            e <- expand e
+            return decl { hsDeclExp = e }
+        f (HsClassDecl srcLoc hsQualType hsDecls) = withSrcLoc srcLoc $ do
+            hsQualType' <- expand hsQualType
+            hsDecls' <- expand hsDecls
+            return (HsClassDecl srcLoc hsQualType' hsDecls')
+        f (HsInstDecl srcLoc hsQualType hsDecls) = withSrcLoc srcLoc $ do
+            hsQualType' <- expand hsQualType
+            hsDecls' <- expand hsDecls
+            return (HsInstDecl srcLoc hsQualType' hsDecls')
+        f (HsPragmaRules rs) = HsPragmaRules <$> mapM renameHsRule rs
+        f prules@HsPragmaSpecialize { hsDeclSrcLoc = srcLoc, hsDeclName = n, hsDeclType = t } = withSrcLoc srcLoc $ do
+            t <- expand t
+            return prules {  hsDeclType = t }
+        f otherHsDecl = return otherHsDecl
 
-renameHsDecl :: HsDecl -> SubTable -> ScopeSM (HsDecl)
-renameHsDecl (HsPatBind srcLoc hsPat hsRhs {-where-} hsDecls) subTable = withSrcLoc srcLoc $ do
-    hsPat'    <- expand hsPat
-    hsDecls'  <- expand hsDecls
-    hsRhs'    <- expand hsRhs
-    let patbind' = (HsPatBind srcLoc hsPat' hsRhs' {-where-} hsDecls')
-    return patbind'
-
-renameHsDecl (HsForeignDecl a b n t) subTable = withSrcLoc a $ do
-    t <- renameHsQualType t subTable
-    return  (HsForeignDecl a b n t)
-
-renameHsDecl (HsForeignExport a b n t) subTable = withSrcLoc a $ do
-    t <- renameHsQualType t subTable
-    return  (HsForeignExport a b n t)
-
-renameHsDecl (HsFunBind hsMatches) subTable = do
-    hsMatches' <- expand hsMatches
-    return (HsFunBind hsMatches')
-
-renameHsDecl (HsTypeSig srcLoc hsNames hsQualType) subTable = withSrcLoc srcLoc $ do
-    hsQualType' <- expand hsQualType
-    return (HsTypeSig srcLoc hsNames hsQualType')
-
-renameHsDecl dl@HsDataDecl { hsDeclContext = hsContext, hsDeclCons = hsConDecls  } subTable = do
-    hsContext' <- expand hsContext
-    hsConDecls' <- expand hsConDecls
-    return dl { hsDeclContext = hsContext', hsDeclCons = hsConDecls' }
-renameHsDecl (HsTypeDecl srcLoc name hsNames t) subTable = withSrcLoc srcLoc $ do
-    t' <- renameHsType' False t undefined
-    return (HsTypeDecl srcLoc name hsNames t')
-renameHsDecl decl@HsActionDecl { hsDeclSrcLoc = srcLoc, hsDeclExp = e }  subTable = withSrcLoc srcLoc $ do
-    e <- renameHsExp e subTable
-    return decl { hsDeclExp = e }
-renameHsDecl (HsClassDecl srcLoc hsQualType hsDecls) subTable = withSrcLoc srcLoc $ do
-    hsQualType' <- expand hsQualType
-    hsDecls' <- expand hsDecls
-    return (HsClassDecl srcLoc hsQualType' hsDecls')
-renameHsDecl (HsInstDecl srcLoc hsQualType hsDecls) subTable = withSrcLoc srcLoc $ do
-    hsQualType' <- expand hsQualType
-    hsDecls' <- expand hsDecls
-    return (HsInstDecl srcLoc hsQualType' hsDecls')
-renameHsDecl (HsPragmaRules rs) subTable = do
-    rs' <- mapM (`renameHsRule` subTable) rs
-    return $ HsPragmaRules rs'
-renameHsDecl prules@HsPragmaSpecialize { hsDeclSrcLoc = srcLoc, hsDeclName = n, hsDeclType = t } subTable = withSrcLoc srcLoc $ do
-    t <- renameHsType t subTable
-    return prules {  hsDeclType = t }
-renameHsDecl otherHsDecl _ = return otherHsDecl
-
-renameHsRule prules@HsRule { hsRuleSrcLoc = srcLoc, hsRuleFreeVars = fvs, hsRuleLeftExpr = e1, hsRuleRightExpr = e2 } subTable = withSrcLoc srcLoc $ do
-    fvs' <- sequence [ T.mapM (`renameHsType` subTable) t  >>= return . (,) n | (n,t) <- fvs]
-    e1' <- renameHsExp e1 subTable
-    e2' <- renameHsExp e2 subTable
+renameHsRule prules@HsRule { hsRuleSrcLoc = srcLoc, hsRuleFreeVars = fvs, hsRuleLeftExpr = e1, hsRuleRightExpr = e2 } = withSrcLoc srcLoc $ do
+    fvs' <- sequence [ T.mapM renameHsType t  >>= return . (,) n | (n,t) <- fvs]
+    e1' <- expand e1
+    e2' <- expand e2
     return prules {  hsRuleFreeVars = fvs', hsRuleLeftExpr = e1', hsRuleRightExpr = e2' }
 
-renameHsQualType :: HsQualType -> SubTable -> ScopeSM (HsQualType)
-renameHsQualType (HsQualType hsContext hsType) subTable = do
-      hsContext' <- expand hsContext
-      hsType' <- expand hsType
-      return (HsQualType hsContext' hsType')
-
-instance Expand HsAsst where
-    expand a = renameHsAsst a ()
 instance Expand HsQualType where
-    expand a = renameHsQualType a ()
-instance Expand HsConDecl where
-    expand a = renameHsConDecl a ()
+    expand (HsQualType hsContext hsType) = do
+        hsContext' <- expand hsContext
+        hsType' <- expand hsType
+        return (HsQualType hsContext' hsType')
 
 instance Expand HsClassHead where
     expand HsClassHead { .. }  = do
         hsClassHeadContext <- expand hsClassHeadContext
-        hsClassHeadArgs <- mapM (flip renameHsType ()) hsClassHeadArgs
+        hsClassHeadArgs <- mapM renameHsType hsClassHeadArgs
         return HsClassHead { .. }
 
-renameHsAsst :: HsAsst -> SubTable -> ScopeSM (HsAsst)
-renameHsAsst (HsAsst hsName1  hsName2s) subTable = do
-      return (HsAsst hsName1 hsName2s)
-renameHsAsst (HsAsstEq t1 t2) subTable = do
-      t1' <- renameHsType t1 subTable  -- for class names
-      t2' <- renameHsType t2 subTable  -- for class names
-      return (HsAsstEq t1' t2')
+instance Expand HsAsst where
+    expand (HsAsstEq t1 t2) = HsAsstEq <$> expand t1 <*> expand t2
+    expand x = return x
 
-renameHsConDecl :: HsConDecl -> SubTable -> ScopeSM (HsConDecl)
-renameHsConDecl cd@(HsConDecl { hsConDeclSrcLoc = srcLoc, hsConDeclName = hsName, hsConDeclConArg = hsBangTypes }) subTable = withSrcLoc srcLoc $ do
-    hsBangTypes' <- expand hsBangTypes
-    return cd { hsConDeclName = hsName, hsConDeclConArg = hsBangTypes' }
-renameHsConDecl cd@HsRecDecl { hsConDeclSrcLoc = srcLoc, hsConDeclName = hsName, hsConDeclRecArg = stuff} subTable = withSrcLoc srcLoc $ do
-    stuff' <- sequence [ do t' <- expand t ; return (ns,t') |  (ns,t) <- stuff]
-    return cd { hsConDeclName = hsName, hsConDeclRecArg = stuff' }
+instance Expand HsConDecl where
+    expand cd@(HsConDecl { hsConDeclSrcLoc = srcLoc, hsConDeclName = hsName, hsConDeclConArg = hsBangTypes }) = withSrcLoc srcLoc $ do
+        hsBangTypes' <- expand hsBangTypes
+        return cd { hsConDeclName = hsName, hsConDeclConArg = hsBangTypes' }
+    expand cd@HsRecDecl { hsConDeclSrcLoc = srcLoc, hsConDeclName = hsName, hsConDeclRecArg = stuff} = withSrcLoc srcLoc $ do
+        stuff' <- sequence [ do t' <- expand t ; return (ns,t') |  (ns,t) <- stuff]
+        return cd { hsConDeclName = hsName, hsConDeclRecArg = stuff' }
 
 instance Expand a => Expand (HsBangType' a) where
     expand x = T.mapM expand x
 
 instance Expand HsType where
-    expand x = renameHsType x ()
-instance Expand HsMatch where
-    expand x = renameHsMatch x ()
+    expand x = renameHsType x
 
 renameHsType = renameHsType' True
-renameHsType' dovar t st = pp (rt t) where
-    rt :: HsType -> ScopeSM HsType
-    rt (HsTyFun hsType1 hsType2) = do
-        hsType1' <- rt hsType1
-        hsType2' <- rt hsType2
-        return (HsTyFun hsType1' hsType2')
-    rt (HsTyTuple hsTypes) = do
-        hsTypes' <- mapM rt hsTypes
-        return (HsTyTuple hsTypes')
-    rt (HsTyUnboxedTuple hsTypes) = do
-        hsTypes' <- mapM rt hsTypes
-        return (HsTyUnboxedTuple hsTypes')
-    rt (HsTyApp hsType1 hsType2) = do
-        hsType1' <- rt hsType1
-        hsType2' <- rt hsType2
-        return (HsTyApp hsType1' hsType2')
-    rt (HsTyForall ts v) = do
-        v <- renameHsQualType v  ()
-        return $ HsTyForall ts v
-    rt (HsTyExists ts v) = do
-        v <- renameHsQualType v  ()
-        return $ HsTyExists ts v
- --   rt (HsTyAssoc) = return HsTyAssoc
---    rt (HsTyEq a b) = return HsTyEq `ap` (flip rt a) `ap` (flip rt b)
- --   rt HsTyExpKind {} _subTable = error "cannot rename HsTyExpKind TypeSyns"
-    rt ty = traverseHsType rt ty
-    pp t | not dovar = t
+renameHsType' dovar t = pp t where
+    pp t | not dovar = return t
     pp t = do
-        t' <- t
         syns <- gets synonyms
-        removeSynonymsFromType syns t'
+        removeSynonymsFromType syns t
 
 -- note that for renameHsMatch, the 'wheres' dominate the 'pats'
 
-renameHsMatch :: HsMatch -> SubTable -> ScopeSM HsMatch
-renameHsMatch (HsMatch srcLoc hsName hsPats hsRhs {-where-} hsDecls) subTable = withSrcLoc srcLoc $ do
-    hsPats' <- expand hsPats
-    hsDecls' <- expand hsDecls
-    hsRhs' <- expand hsRhs
-    return (HsMatch srcLoc hsName hsPats' hsRhs' {-where-} hsDecls')
+instance Expand HsMatch where
+    expand (HsMatch srcLoc hsName hsPats hsRhs {-where-} hsDecls) = withSrcLoc srcLoc $ do
+        hsPats' <- expand hsPats
+        hsDecls' <- expand hsDecls
+        hsRhs' <- expand hsRhs
+        return (HsMatch srcLoc hsName hsPats' hsRhs' {-where-} hsDecls')
+
+instance Expand a => Expand (HsRhs' a) where
+    expand = T.mapM expand
 
 instance Expand HsPat where
-    expand x = renameHsPat x ()
-instance Expand HsRhs where
-    expand x = renameHsRhs x ()
-
-renameHsPat :: HsPat -> SubTable -> ScopeSM (HsPat)
-renameHsPat (HsPTypeSig srcLoc hsPat qt) subTable = withSrcLoc srcLoc $ do
-    hsQualType' <- renameHsQualType qt subTable
-    hsPat' <- expand hsPat
-    return (HsPTypeSig srcLoc hsPat' hsQualType')
-renameHsPat p subTable = traverseHsPat (flip renameHsPat subTable) p
-
-renameHsRhs :: HsRhs -> SubTable -> ScopeSM HsRhs
-renameHsRhs (HsUnGuardedRhs hsExp) subTable = do
-      hsExp' <- expand hsExp
-      return (HsUnGuardedRhs hsExp')
-renameHsRhs (HsGuardedRhss hsGuardedRhss) subTable = do
-      hsGuardedRhss' <- expand hsGuardedRhss
-      return (HsGuardedRhss hsGuardedRhss')
+    expand (HsPTypeSig srcLoc hsPat qt) = withSrcLoc srcLoc $ do
+        hsQualType' <- expand qt
+        hsPat' <- expand hsPat
+        return (HsPTypeSig srcLoc hsPat' hsQualType')
+    expand p = traverseHsPat expand p
 
 instance Expand HsExp where
-    expand x = renameHsExp x ()
-
-renameHsExp :: HsExp -> SubTable -> ScopeSM HsExp
-renameHsExp (HsLambda srcLoc hsPats hsExp) subTable = withSrcLoc srcLoc $ do
-    hsPats' <- expand hsPats
-    hsExp' <- expand hsExp
-    return (HsLambda srcLoc hsPats' hsExp')
-renameHsExp (HsLet hsDecls hsExp) subTable = do
-    hsDecls' <- expand hsDecls
-    hsExp' <- expand hsExp
-    return (HsLet hsDecls' hsExp')
-renameHsExp (HsCase hsExp hsAlts) subTable = do
-    hsExp' <- expand hsExp
-    hsAlts' <- expand hsAlts
-    return (HsCase hsExp' hsAlts')
-renameHsExp (HsRecConstr hsName hsFieldUpdates) subTable = do
-    hsFieldUpdates' <- expand hsFieldUpdates
-    return (HsRecConstr hsName hsFieldUpdates')
-renameHsExp (HsRecUpdate hsExp hsFieldUpdates) subTable = do
-    hsExp' <- expand hsExp
-    hsFieldUpdates' <- expand hsFieldUpdates
-    return (HsRecUpdate hsExp' hsFieldUpdates')
-renameHsExp (HsListComp hsExp hsStmts) _
-    = HsListComp <$> expand hsExp <*> expand hsStmts
-renameHsExp (HsExpTypeSig srcLoc hsExp hsQualType) subTable = do
-    hsExp' <- expand hsExp
-    hsQualType' <- expand hsQualType
-    return (HsExpTypeSig srcLoc hsExp' hsQualType')
-renameHsExp e _ = traverseHsExp expand e
+    expand x = f x where
+        f (HsLambda srcLoc hsPats hsExp) = withSrcLoc srcLoc $ do
+            hsPats' <- expand hsPats
+            hsExp' <- expand hsExp
+            return (HsLambda srcLoc hsPats' hsExp')
+        f (HsLet hsDecls hsExp) = do
+            hsDecls' <- expand hsDecls
+            hsExp' <- expand hsExp
+            return (HsLet hsDecls' hsExp')
+        f (HsCase hsExp hsAlts) = do
+            hsExp' <- expand hsExp
+            hsAlts' <- expand hsAlts
+            return (HsCase hsExp' hsAlts')
+        f (HsRecConstr hsName hsFieldUpdates) = do
+            hsFieldUpdates' <- expand hsFieldUpdates
+            return (HsRecConstr hsName hsFieldUpdates')
+        f (HsRecUpdate hsExp hsFieldUpdates) = do
+            hsExp' <- expand hsExp
+            hsFieldUpdates' <- expand hsFieldUpdates
+            return (HsRecUpdate hsExp' hsFieldUpdates')
+        f (HsListComp a b) = HsListComp <$> expand a <*> expand b
+        f (HsExpTypeSig srcLoc a b) = HsExpTypeSig srcLoc <$> expand a <*> expand b
+        f e = traverseHsExp expand e
 
 instance Expand HsAlt where
     expand (HsAlt srcLoc hsPat hsGuardedAlts {-where-} hsDecls) = withSrcLoc srcLoc $ do
@@ -290,9 +192,8 @@ instance Expand HsAlt where
         hsGuardedAlts' <- expand hsGuardedAlts
         return (HsAlt srcLoc hsPat' hsGuardedAlts' hsDecls')
 
-instance Expand HsGuardedRhs where
-    expand (HsGuardedRhs srcLoc hsExp1 hsExp2) = withSrcLoc srcLoc $ do
-        HsGuardedRhs srcLoc <$> expand hsExp1 <*> expand hsExp2
+instance Expand a => Expand (HsGuardedRhs' a) where
+    expand = T.mapM expand
 
 instance Expand HsStmt where
     expand (HsGenerator sl a b) = HsGenerator sl <$> expand a <*> expand b
