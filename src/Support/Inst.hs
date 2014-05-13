2b1026d3819323c43965de49502fd173c3eeda1c
Sergei Trofimovich <slyfox@community.haskell.org>
2014-05-14 03:19:19 +0900

    src/Support/Inst.hs: don't define instances present in ghc-7.8
    Ignore-this: 846e90c9a24c9eccc5c7358078529a72
    
    [ 85 of 183] Compiling Support.Inst     ( src/Support/Inst.hs, src/Support/Inst.o )
    
    src/Support/Inst.hs:7:10:
        Duplicate instance declarations:
          instance [overlap ok] Foldable ((,) a)
            -- Defined at src/Support/Inst.hs:7:10
          instance Foldable ((,) a) -- Defined in 'Data.Foldable'
    
    src/Support/Inst.hs:9:10:
        Duplicate instance declarations:
          instance [overlap ok] Traversable ((,) a)
            -- Defined at src/Support/Inst.hs:9:10
          instance Traversable ((,) a) -- Defined in 'Data.Traversable'
    
    darcs-hash:20140513181919-6895e-2eae049a6cbeceb9798c9fc5e92ae400a266b97c.gz

--- src/Support/Inst.hs
+++ src/Support/Inst.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE CPP #-}
 module Support.Inst where
 
 import Control.Applicative
 import Data.Foldable
 import Data.Traversable
 
+#if __GLASGOW_HASKELL__ < 708
 instance Foldable ((,) a) where
     foldMap = foldMapDefault
 instance Traversable  ((,) a) where
     traverse f (x,y) = (,) x <$> f y
+#endif
 
 instance Functor ((,,) a b) where
     fmap = fmapDefault
