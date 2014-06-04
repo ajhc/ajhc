71129fb61b59aa5cbd908c8b36c8b34c054eb60d
John Meacham <john@repetae.net>
2014-06-04 16:45:08 +0900

    add tests just to check haskell98 and haskell2010 are exporting all the right modules.
    Ignore-this: dd5e786837052bbf8c75c984792f22ad
    
    darcs-hash:20140604074508-1a7c6-c11d4630345e9b192dab329160165265bb312fe6.gz

---
+++ regress/tests/2_language/haskell2010.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE Haskell2010 #-}
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.Char
+import Data.Complex
+import Data.Int
+import Data.Ix
+import Data.List
+import Data.Maybe
+import Data.Ratio
+import Data.Word
+import Foreign
+import Foreign.C
+import Foreign.C.Error
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.ForeignPtr
+import Foreign.Marshal
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
+import Foreign.Marshal.Error
+import Foreign.Marshal.Utils
+import Foreign.Ptr
+import Foreign.StablePtr
+import Foreign.Storable
+import Numeric
+import Prelude
+import System.Environment
+import System.Exit
+import System.IO
+import System.IO.Error
+
+main :: IO ()
+main = return ()
---
+++ regress/tests/2_language/haskell98.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE Haskell98 #-}
+import Prelude
+import Numeric
+import Array
+import CPUTime
+import Char
+import Complex
+import Directory
+import IO
+import Ix
+import List
+import Locale
+import Maybe
+import Monad
+import Random
+import Ratio
+import System
+import Time
+
+main :: IO ()
+main = return ()
